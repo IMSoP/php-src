1dd6f64c32cb40ea31b79197f88790749fb4a84d
Rowan Collins <rowan.collins@gmail.com>
2019-03-04 23:06:54 +0000

    Initial error implementation, using "final" as keyword

--- Zend/zend_object_handlers.c
+++ Zend/zend_object_handlers.c
@@ -779,6 +779,8 @@ call_getter:
 			zend_throw_error(NULL, "Typed property %s::$%s must not be accessed before initialization",
 				ZSTR_VAL(prop_info->ce->name),
 				ZSTR_VAL(name));
+		} else if ( zobj->ce->ce_flags & ZEND_ACC_FINAL ) {
+			zend_throw_error(NULL, "Cannot access undefined property $%s on locked class %s", ZSTR_VAL(name), ZSTR_VAL(zobj->ce->name));
 		} else {
 			zend_error(E_NOTICE,"Undefined property: %s::$%s", ZSTR_VAL(zobj->ce->name), ZSTR_VAL(name));
 		}
@@ -883,6 +885,11 @@ write_std_property:
 
 			ZVAL_COPY_VALUE(variable_ptr, value);
 		} else {
+			if ( zobj->ce->ce_flags & ZEND_ACC_FINAL ) {
+				zend_throw_error(NULL, "Cannot write undefined property $%s on locked class %s", ZSTR_VAL(name), ZSTR_VAL(zobj->ce->name));
+				variable_ptr = &EG(error_zval);
+				goto exit;
+			}
 			if (!zobj->properties) {
 				rebuild_object_properties(zobj);
 			}
